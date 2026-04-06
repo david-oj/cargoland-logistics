c67d41cd5c9fe60339e56fa73009f84a904f0785
david <davidojo.dev@gmail.com>
2026-04-06 15:22:11 +0100

    feat: initial commit

---
+++ components/layout/Heading.tsx
@@ -0,0 +1,15 @@
+
+
+const Heading = () => {
+
+    return (
+        <div className="">
+            <div className="">
+
+            <p className="">Support@cargoland.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Heading;
